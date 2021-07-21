10ed34ddbc76e9c3881ca7168cbcb908c17f6a8c
Musi340 <nyamusimercy61@gmail.com>
2021-07-21 13:52:58 +0200

    task 7

--- task_0.7.js
+++ task_0.7.js
@@ -1,9 +1,8 @@
-//Converts Celsius to Fahrenheit
 function celToFahrenheit(temp1)
 {
     return 32 + temp1 * 1.8;
 }
-//Converts Fahrenheit to Celsius
+
 function fahrenheitToCel(temp2)
 {
     return (temp2 - 32)/ 1.8;
